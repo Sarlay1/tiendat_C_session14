3f999260f03eef046c5971d6345d33a97d768560
tiendat <dat.70.k3@gmail.com>
2024-12-11 22:01:23 +0700

    bai 1

---
+++ ss14_01.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str[100];
+
+    printf("Nhap mot chuoi: ");
+    fgets(str, sizeof(str), stdin);
+
+  
+    str[strcspn(str, "\n")] = '\0';
+
+    printf("Chuoi vua nhap: %s\n", str);
+    printf("Do dai chuoi: %zu\n", strlen(str));
+
+    return 0;
+}
+
